b89668b4988037aa226225b8418a6e6c4ab2fff1
Cedric Millard <cedric.millard@gmail.com>
2020-11-21 00:00:18 +0100

    v2

--- Unlock.js
+++ Unlock.js
@@ -2,6 +2,7 @@ function addCard(numCard)
 {
     var cln = document.getElementById("TemplateCard").cloneNode(false);
     cln.id = numCard;
+    cln.name = numCard;
     cln.src = "./"+gameName+"/"+gameName+"V_" + numCard + ".png";
     cln.style.display = "inline-block";
     document.getElementById("CardDeck").insertBefore(cln, document.getElementById("end"));
@@ -11,6 +12,7 @@ function returnCard(numCard)
 {
     var cln = document.getElementById("TemplateCardV").cloneNode(false);
     cln.id = numCard+"V";
+    cln.name = numCard;
     cln.src = "./"+gameName+"/"+gameName+"_" + numCard + ".png";
     cln.style.display = "inline-block";
     document.getElementById(numCard).style.display = "none";
@@ -19,8 +21,18 @@ function returnCard(numCard)
 
 function discardCard(numCard)
 {
-    if(confirm("Would you like to discard card "+numCard))
-        document.getElementById(numCard).style.display = "none";
+    if(confirm("Would you like to discard card "+numCard+" ?"))
+    {
+        document.getElementById(numCard+"V").style.display = "none";
+        var cln = document.getElementById("TemplateCardD").cloneNode(false);
+        cln.id = numCard+"D";
+        cln.name = numCard;
+        cln.src = "./"+gameName+"/"+gameName+"_" + numCard + ".png";
+        cln.style.display = "inline-block";
+        document.getElementById("DismissedCard").insertBefore(cln, document.getElementById("endD"));
+        cln.width=cln.width/4;
+
+    }
 }
 
 var start = 0
